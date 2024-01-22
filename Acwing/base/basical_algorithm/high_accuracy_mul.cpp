90e1e020807584704b686fcdcd4333d3f90aad46
kahvia-d <xuedongtan@163.com>
2024-01-22 19:45:14 +0800

    补充了高精度乘法的注释。

--- Acwing/base/basical_algorithm/high_accuracy_mul.cpp
+++ Acwing/base/basical_algorithm/high_accuracy_mul.cpp
@@ -5,10 +5,12 @@
 #include <vector>
 using namespace std;
 
-
+//乘法的实质就是123 * 4 = 100 * 4 + 20 * 4 + 3 * 4.结果的个位诞生于最低位乘以4，十位诞生于十位乘以4加上个位进的位
+//以此类推。
 vector<int> mul(vector<int> &A, int b){
     vector<int> C;
     int t = 0;
+    //大数每一位乘以小数，并加上低位进的位，得到的结果最后一位，为当前位的数，结果的其余位都是进位。
     for(int i = 0; i < A.size(); i++){
         t += A[i] * b;
         C.push_back(t % 10);
